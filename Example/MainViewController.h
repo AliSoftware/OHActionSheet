2665924adc6c84670d9ded9ee9ec74b959667489
Olivier Halligon <olivier@halligon.net>
2014-07-02 16:09:41 +0200

    Modernized Example project

---
+++ Example/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  OHAlertsExample
+//
+//  Created by Olivier Halligon on 02/07/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Example/MainViewController.m
@@ -0,0 +1,98 @@
+//
+//  MainViewController.m
+//  OHAlertsExample
+//
+//  Created by Olivier Halligon on 02/07/2014.
+//
+//
+
+#import "MainViewController.h"
+#import "OHActionSheet.h"
+
+@interface MainViewController ()
+@property (nonatomic, retain) IBOutlet UILabel *status;
+@end
+
+@implementation MainViewController
+
+
+-(IBAction)showSheet1
+{
+	NSArray* flavours = [NSArray arrayWithObjects:@"chocolate",@"vanilla",@"strawberry",nil];
+	
+	[OHActionSheet showSheetInView:self.view
+                             title:@"Ice cream?"
+                 cancelButtonTitle:@"Maybe later"
+            destructiveButtonTitle:@"No thanks!"
+                 otherButtonTitles:flavours
+                        completion:^(OHActionSheet *sheet, NSInteger buttonIndex)
+	 {
+		 NSLog(@"button tapped: %ld", (long)buttonIndex);
+		 if (buttonIndex == sheet.cancelButtonIndex) {
+			 self.status.text = @"Your order has been postponed";
+		 }
+         else if (buttonIndex == sheet.destructiveButtonIndex)
+         {
+			 self.status.text = @"Your order has been cancelled";
+		 }
+         else
+         {
+			 NSString* flavour = [flavours objectAtIndex:(buttonIndex-sheet.firstOtherButtonIndex)];
+			 self.status.text = [NSString stringWithFormat:@"You ordered a %@ ice cream.",flavour];
+		 }
+	 }];
+}
+
+-(IBAction)showSheet2
+{
+	NSArray* flavours = [NSArray arrayWithObjects:@"apple",@"pear",@"banana",nil];
+    
+	[[[OHActionSheet alloc] initWithTitle:@"What's your favorite fruit?"
+                        cancelButtonTitle:@"Don't care"
+                   destructiveButtonTitle:nil
+                        otherButtonTitles:flavours
+                               completion:^(OHActionSheet *sheet, NSInteger buttonIndex)
+      {
+          NSLog(@"button tapped: %ld",(long)buttonIndex);
+          if (buttonIndex == sheet.cancelButtonIndex) {
+              self.status.text = @"You didn't answer the question";
+          }
+          else if (buttonIndex == -1)
+          {
+              self.status.text = @"The action sheet timed out";
+          }
+          else
+          {
+              NSString* fruit = [flavours objectAtIndex:(buttonIndex-sheet.firstOtherButtonIndex)];
+              self.status.text = [NSString stringWithFormat:@"Your favorite fruit is %@.",fruit];
+          }
+      }] showInView:self.view withTimeout:8 timeoutButtonIndex:-1];
+}
+
+- (IBAction)showSheet3:(UIBarButtonItem *)sender
+{
+    [OHActionSheet showSheetInView:(id)sender
+                             title:@"Sheet from Bar"
+                 cancelButtonTitle:@"Cancel"
+            destructiveButtonTitle:@"Delete"
+                 otherButtonTitles:@[@"Save"]
+                        completion:^(OHActionSheet *sheet, NSInteger buttonIndex)
+     {
+         NSLog(@"Button tapped: %ld", (long)buttonIndex);
+         if (buttonIndex == sheet.cancelButtonIndex)
+         {
+             self.status.text = @"You cancelled";
+         }
+         else if (buttonIndex == sheet.destructiveButtonIndex)
+         {
+             self.status.text = @"You chose to delete";
+         }
+         else
+         {
+             self.status.text = @"You chose to save";
+         }
+         
+     }];
+}
+
+@end
